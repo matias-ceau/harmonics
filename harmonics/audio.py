ab1fe56d412dbe4aad2fb4bfa8806d0faae3c479
Matias Ceau <mceau@u862.local>
2022-05-06 17:30:41 +0200

    s

--- harmonics/audio.py
+++ harmonics/audio.py
@@ -8,37 +8,44 @@ import numpy as np
 def get_enveloppe(s,data,length=0.02,plot=True):
     x = np.array([i for i in range(0,len(data),int(0.02*s))])
     y = np.array([0]+[np.max(data[x[i]:x[i+1]]) for i in range(len(x)-1)])
-    x = x/x.max()
+    x1 = x/s
+    x2 = x/x.max()
     y = y/y.max()
-    if plot: audio_plot(x,y,'time','amplitude')
-    return np.vstack((x,y))
+    if plot: audio_plot(x1,y,'time','amplitude')
+    return np.vstack((x1,x2,y))
 
-def get_sine(duration,F,A=4096,aR=1,fR=1,sample_rate=44100,plot=True):
+def get_sine(duration,F,A=4096,aR=1,fR=1,phase=0,sample_rate=44100,plot=True):
     t = np.arange(duration*int(sample_rate)) # Time axis
-    wave = (aR*A)*np.sin(2*np.pi*(fR*F/sample_rate)*t)
+    wave = aR*A*np.sin(2*np.pi*(fR*F/sample_rate)*t-phase)
     if plot: audio_plot(np.arange(0,len(wave)),wave,'time (s)','amplitude')
     return wave
 
 def get_harmonics(s,data,plot=True):
       t    = np.arange(data.shape[0])
       freq = np.fft.rfftfreq(t.shape[-1])*s
-      sp   = np.fft.rfft(data) 
-      df = pd.DataFrame(np.vstack((freq,abs(sp.real))).T,
-                        columns=['frequency','amplitude'])#.sort_values('amplitude',ascending=False)
+      sp   = np.fft.rfft(data)
+      amplitude = np.abs(sp)
+      phase = np.angle(sp)
+      df = pd.DataFrame(np.vstack((freq,amplitude,phase)).T,
+                        columns=['frequency','amplitude','phase'])#.sort_values('amplitude',ascending=False)
       max_freq = df[df.amplitude == df.amplitude.max()].frequency
       a, _ = find_peaks(df.amplitude,
                         threshold=df.amplitude.max()*0.01, 
                           distance=int(max_freq/900*100))
       f = df.loc[a,:]
       f.sort_values('frequency',inplace=True)
-      harmonics = [(round(a,2),round(b,2)) for a,b in zip(f.frequency,f.amplitude)]
+      harmonics = [(round(a,3),round(b,3),round(c,3)) for a,b,c in zip(f.frequency,
+                                                                       f.amplitude,
+                                                                       f.phase)]
       fundamental = f[f.amplitude == f.amplitude.max()].frequency.to_numpy().max()
-      rel_h = [(round(a/fundamental,2), round(b/f.amplitude.max(),2)) for a,b in harmonics]
+      rel_h = [(round(a/fundamental,3), 
+                round(b/f.amplitude.max(),3),
+                round(c,3)) for a,b,c in harmonics]
       note = h.Note(str(fundamental)+'Hz')
       if plot:
           freq = freq[freq<10*fundamental]
-          ampl = abs(sp.real)[:len(freq)]
-          audio_plot(freq,ampl,'Frequency (Hz)','Amplitude',scatter=([a for a,b in harmonics],[b for a,b in harmonics]))
+          ampl = amplitude[:len(freq)]
+          audio_plot(freq,ampl,'Frequency (Hz)','Amplitude',scatter=([a for a,b,c in harmonics],[b for a,b,c in harmonics]))
       return rel_h, note
       
 def audio_plot(x,y,xlabel='',ylabel='',title='',show='all',figsize=(10,4),scatter=False):
@@ -60,22 +67,30 @@ class Sampler:
         self.file           = file
         self._analyzed      = {'file':[],'data':[],'rel_harmonics':[],'note':[],'offpitch':[],'enveloppe':[],
                                'frequency':[],'duration':[]}
-        self.overtones      = False
-        self.enveloppe      = False
+        self._overtones     = False
+        self._enveloppe     = False
         
     @property
-    def analyzed(self):
+    def df(self):
         return pd.DataFrame({k:pd.Series(v) for k,v in self._analyzed.items()})
     
-    def set_overtones(self,i):
-        if type(i) == int: self.overtones = self.analyzed.loc[i,'rel_harmonics']
-        elif i == False: self.overtones = False
-        return self.overtones
+    @property
+    def overtones(self):
+        return self._overtones
     
-    def set_enveloppe(self,i):
-        if type(i) == int: self.enveloppe = self.analyzed.loc[i,'enveloppe']
-        elif i == False: self.enveloppe = False
-        return self.enveloppe
+    @overtones.setter
+    def overtones(self,i):
+        if type(i) == int: self._overtones = self.df.loc[i,'rel_harmonics']
+        elif i == False: self._overtones = False
+    
+    @property
+    def enveloppe(self):
+        return self._enveloppe
+    
+    @enveloppe.setter
+    def enveloppe(self,i):
+        if type(i) == int: self._enveloppe = self.df.loc[i,'enveloppe']
+        elif i == False: self._enveloppe = False
             
         
     def get_wave(self,duration,
@@ -84,26 +99,36 @@ class Sampler:
                  wav=True,
                  overtones='def',
                  enveloppe='def',
+                 env_stretch=False,
                  filename='name.wav',
+                 autodecay=20,
                  show='all',
                  s=44100):
         if freq: self.frequency = freq
         if overtones == 'def': overtones = self.overtones
         if enveloppe == 'def': enveloppe = self.enveloppe
-        if type(overtones) == int: overtones = self.set_overtones(overtones)
-        if type(enveloppe) == int: enveloppe = self.set_enveloppe(enveloppe)
+        if type(overtones) == int: self.overtones = overtones ; overtones = self.overtones
+        if type(enveloppe) == int: self.enveloppe = enveloppe ; enveloppe = self.enveloppe
         if not overtones:
             wave = get_sine(duration,self.frequency,self.amplitude, sample_rate=s)
         else:
             print('added overtones')
-            wave = np.sum([get_sine(duration,A=self.amplitude,F=self.frequency,aR=a,fR=f,sample_rate=s,plot=False) for f,a in overtones],axis=0)
+            wave = np.sum(\
+            [get_sine(duration,A=self.amplitude,F=self.frequency,
+                      aR=a,fR=f,phase=p,sample_rate=s,plot=False) for f,a,p in overtones],axis=0)
         #def get_sine(duration,F,A=4096,aR=1,fR=1,sample_rate=44100,plot=True)
         if type(enveloppe) == np.ndarray:
-            print('WELL HELLP')
-            scaled_env = np.interp(np.arange(0,len(wave)),
-                                    enveloppe[0,:]*len(wave),
-                                    enveloppe[1,:])
-            print(len(wave),len(scaled_env))
+            t = np.arange(0,len(wave))
+            if env_stretch:
+                scaled_env = np.interp(t,enveloppe[1,:]*len(wave),enveloppe[2,:])
+            else:
+                scaled_env = np.interp(t,enveloppe[0,:]*s,enveloppe[2,:])
+                plt.plot(scaled_env[:int(s*0.02)-100])
+                if autodecay:
+                    idx = int(s*autodecay/1000) 
+                    scaled_env =  np.concatenate((scaled_env[:-idx],
+                                                  np.linspace(scaled_env[-idx],0,idx)))
+                plt.plot(scaled_env[:int(s*0.02)-100])
             wave *= scaled_env
         if plot:audio_plot(np.arange(len(wave))/self.sample_rate,wave,'time','amplitude',show=show)
         
