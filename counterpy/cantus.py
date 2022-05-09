5824d7a8574e0c8fb5e7508ba17857ca76b267ca
Matias Ceau <mceau@u862.local>
2022-05-09 17:27:20 +0200

    .

--- counterpy/cantus.py
+++ counterpy/cantus.py
@@ -1,8 +1,9 @@
 import numpy as np
-import random
 from typing import List, Callable, Tuple
-import time
 import pandas as pd
+import random
+import time
+import os
 
 Sequence    = np.ndarray
 Population  = List[Sequence]
@@ -12,56 +13,85 @@ MAX_RANGE = 12
 MAX_LEAP  = 8
 NOTES = np.hstack([np.arange(i+36,80,12) for i in [0,2,4,5,7,9,11]])
 
+_folder = os.path.dirname(os.path.dirname(__file__))
 
 ### COMMON
-def no_repeted_notes(sequence: Sequence,*args) -> bool:
-    '''True if there isn't any repeted notes'''
-    return (np.diff(sequence) != 0).all()
 
-def all_notes(sequence: Sequence,*args) -> bool:
+def no_repeted_notes(sequence: Sequence,*args) -> int:
+    '''True if there isn't any repeted notes'''
+    if (np.diff(sequence) != 0).all(): return 1
+    else: return 0
+
+
+def all_notes(sequence: Sequence,*args) -> int:
     '''if sequence is longer than 10, it should contain all (ie 7) the notes'''
     if sequence.shape[0] >=10:
-        return np.unique(sequence%12).shape[0] == 7
+        if np.unique(sequence%12).shape[0] == 7: return 1
+        else: return 0
     else:
-        return True
+        return 1
 
-def mostly_stepwise(sequence: Sequence,*args) -> bool:
+
+def mostly_stepwise(sequence: Sequence,*args) -> int:
     '''True if 50% of intervals are less or equal to 2 semitones'''
     test = np.abs(np.diff(sequence))
-    return (test[test <= 2].shape[0] >= sequence.shape[0]/2)
+    if (test[test <= 2].shape[0] >= sequence.shape[0]/2):
+        return 1
+    else: return 0
+
     
-def single_climax(sequence: Sequence,*args) -> bool:
+def single_climax(sequence: Sequence,*args) -> int:
     '''True if climax appears only once'''
     idx = np.where(np.abs(sequence) == np.abs(sequence).max())[0]
-    return (sequence[sequence == sequence[idx]].shape[0] == 1)
+    if (sequence[sequence == sequence[idx]].shape[0] == 1):
+        return 1
+    else: return 0
 
-def leap_range_ok(sequence: Sequence,*args) -> bool:
+
+def leap_range_ok(sequence: Sequence,*args) -> int:
     '''True if no leap above 8 semitones'''
     test = np.abs(np.diff(sequence))
-    return test[test > MAX_LEAP].shape[0] == 0
+    if test[test > MAX_LEAP].shape[0] == 0:
+        return 1
+    else:
+        return 0
+
                  
-def leap_frequency_ok(sequence: Sequence,*args) -> bool:
+def leap_frequency_ok(sequence: Sequence,*args) -> int:
     '''True if there isn't more than 3 consecutive leaps (more than 3 semitones)'''
-    return (0 not in np.diff(np.diff(np.where( np.abs(np.diff(sequence)) >= 3)[0])))
+    if (0 not in np.diff(np.diff(np.where( np.abs(np.diff(sequence)) >= 3)[0]))):
+        return 1
+    else:
+        return 0
 
-def range_is_ok(sequence: Sequence,*args) -> bool:
+
+def range_is_ok(sequence: Sequence,*args) -> int:
     '''True if range is less than specified interval (in semitones)'''
-    return sequence.max()-sequence.min() <= MAX_RANGE
+    if sequence.max()-sequence.min() <= MAX_RANGE:
+        return 1
+    else:
+        return 0
 
-def leap_balance_ok(sequence: Sequence,*args) -> bool:
+
+def leap_balance_ok(sequence: Sequence,*args) -> int:
      '''True if eventual leaps greater than p4 are counterbalanced by leaps in opposite directions (first tests the presence of such leaps, then identify if they are counterbalanced (half of sum of differences is still under p4)'''
-     if np.abs(np.diff(sequence).max()) <= 5: return True
+     if np.abs(np.diff(sequence).max()) <= 5: return 1
      else:
          idx = np.where(np.abs(np.diff(sequence)) >= 5)[0]
          test = [np.diff(sequence[i:i+3]) for i in idx]
-         return np.sum([np.sign(np.prod(i)) == -1 for i in test]) + \
-                 np.sum([np.abs(i[-1])>=5 for i in test])             == len(test)
+         if np.sum([np.sign(np.prod(i)) == -1 for i in test]) +  np.sum([np.abs(i[-1])>=5 for i in test])  == len(test):
+             return 1
+         else:
+             return 0
    
 ### CANTUS SPECIFIC
 
-def ante_is_leading(sequence: Sequence, *args) -> bool:
+def ante_is_leading(sequence: Sequence, *args) -> int:
     '''True if antepenultian note is the leading tone above or below (based on assumption that all notes are diatonic'''
-    return  0 < np.abs(sequence[-1]-sequence[-2]) <= 2
+    if  0 < np.abs(sequence[-1]-sequence[-2]) <= 2:
+        return 1
+    else:
+        return 0
 
 #### CANTUS FIRMUS
 '''
@@ -93,63 +123,82 @@ cantus_scores = [b for a,b in cantus_list]
 max_fit = len(cantus_list)
 
 
-def generate_sequence(length: int, key_center: int, restricted_notes) -> Sequence:
+def generate_sequence(length: int, 
+                      key_center: int, 
+                      restricted_notes: np.ndarray) -> Sequence:
     #start = 60
     #end   = random.choice([60,67])
     return np.concatenate(([key_center],np.random.choice(restricted_notes,size=length-2),[key_center]))
 
-def generate_population(size: int, length: int, key_center: int, restricted_notes) -> Population:
+
+def generate_population(size: int,
+                        length: int,
+                        key_center: int,
+                        restricted_notes: np.ndarray) -> Population:
     return [generate_sequence(length=length, key_center=key_center, restricted_notes=restricted_notes) for _ in range(size)]
 
-def fitness(sequence: Sequence, functions: FitnessList, key: int) -> int:
+
+def fitness(sequence: Sequence,
+            functions: FitnessList,
+            key: int) -> int:
     if key not in range(128): raise ValueError("Key should be a valid midi note (0-127")
-    score = np.sum(np.array([f(sequence,key) for f in functions]))
+    score = sum([f(sequence,key) for f in functions])
     return score
 
-def pair_selection(population,
-                   weights) -> Population:
-    return random.choices(
-           population = population,
-           weights    = [i*max_fit/3 for i in weights],
-           k=2
+
+def pair_selection(population: Population,
+                   weights:    list) -> np.array:
+    weights =  np.array([i**2 for i in weights],dtype=float)
+    p = weights/weights.sum()
+    return np.random.choice(
+           population,
+           size=2,
+           p = p,
            )
 
 def single_point_crossover(a: Sequence, b: Sequence) -> Tuple[Sequence, Sequence]:
     if len(a) != len(b):
         raise ValueError('Sequence length must match!')
     if len(a) == 2: return a,b
-    p = random.randint(1, len(a)-1)
+    p = np.random.randint(1, len(a)-1)
     return np.hstack((a[0:p],b[p:])),np.hstack((b[0:p],a[p:]))
 
-def mutation(sequence: Sequence, restricted_notes, num: int = 1, probability: float = 0.5) -> Sequence:
+
+def mutation(sequence: Sequence,
+             restricted_notes,
+             num: int = 1,
+             probability: float = 0.5) -> Sequence:
     for _ in range(num):
-        index = random.randrange(1,sequence.shape[0]-1)
-        note  = random.choice(restricted_notes)
-        sequence[index] = sequence[index] if random.random() > probability else note
+        index = np.random.choice(np.arange(1,sequence.shape[0]-1))
+        note  = np.random.choice(restricted_notes)
+        sequence[index] = sequence[index] if np.random.random() > probability else note
     return sequence
 
 
 def run_evolution(
-        sequence_length: int = 10,
-        key_center:      int = 60,
-        fitness_limit  = max_fit,
-        population_size: int = 100,
-        fitness_list   = cantus_func,
-        #fitness_weight = cantus_scores,
-        fitness_func   = fitness,
-        sequence_gen   = generate_sequence,
-        population_gen = generate_population,
-        pair           = pair_selection,
-        crossover      = single_point_crossover,
-        mut            = mutation,
-        mut_proba: float = 0.5,
-        mut_nb:    int   = 3,
-        generation_limit = 1000) -> Tuple[Population, int]: 
+        sequence_length: int   = 16,
+        key_center:      int   = 60,
+        fitness_limit:   float = max_fit,
+        population_size: int   = 20,
+        fitness_list:   FitnessList = cantus_func,
+        fitness_func:   Callable = fitness,
+        sequence_gen:   Callable = generate_sequence,
+        population_gen: Callable = generate_population,
+        pair:           Callable = pair_selection,
+        crossover:      Callable = single_point_crossover,
+        mut:            Callable = mutation,
+        mut_tresh:      float = 0.5,
+        mut_nb:         int   = 3,  
+        cross_rate:      float  = 1,
+        generation_limit: int = 1000) -> Tuple[Population, int]:
     
-    start = time.time()
+    start: float = time.time()
     if key_center not in [60,62,64,65,67,69,71]: raise ValueError('Wrong octave for generation!')
-    climax = random.choice([i for i in NOTES if MAX_LEAP < np.abs(key_center - i) <= MAX_RANGE])
-    restricted_notes = [i for i in NOTES if (np.abs(climax-i) <= MAX_RANGE) and (np.abs(key_center-i) <= MAX_RANGE)]
+    climax = np.random.choice(np.array([i for i in NOTES if MAX_LEAP 
+                               < np.abs(key_center - i) <= 
+                                     MAX_RANGE],dtype=int))
+    restricted_notes = np.array([i for i in NOTES if (np.abs(climax-i) <= MAX_RANGE) 
+                                 and (np.abs(key_center-i) <= MAX_RANGE)],dtype=int)
     population = generate_population(size=population_size,
                                      length=sequence_length,
                                      key_center=key_center,
@@ -157,8 +206,8 @@ def run_evolution(
     
     max_scores = []
     for i in range(generation_limit):
-        popD = pd.DataFrame({"sequence":population,
-                          "score"    :[fitness_func(sequence=seq,
+        popD = pd.DataFrame({"sequence" :population,
+                             "score"    :[fitness_func(sequence=seq,
                                                    functions=fitness_list,
                                                    key=key_center) for seq in population]})
         popD.sort_values("score",inplace=True,ascending=False)
@@ -168,29 +217,38 @@ def run_evolution(
         pp = '\n'.join([f"""{str(popD.loc[i,"sequence"])} {popD.loc[i,"score"]}""" for i in popD.index])
         print(f"""Generation {i}\n{pp}""")
         if popD.score.max() >= fitness_limit:
-            end = time.time()
+            end: float = time.time()
             print(f'Found a cantus in {round((end -start)*1000,2)} ms!')
-            print(popD[popD.score == popD.score.max()].sequence)
+            cantus = popD[popD.score == popD.score.max()].sequence.tolist()[0]
+            print(cantus)
+            with open(os.path.join(_folder,'data','counter_point_arrays',f'{key_center}_{sequence_length}_{1000*end}.npy'), 'wb') as f:
+                np.save(f, cantus)
             break
         
         next_generation = popD.sequence.tolist()[:2]
         
-        print('Generating crossover mutation...')
+        print('Generating crossover and single point mutations...')
         if np.sum(popD.score) == 0: popD.score += 0.01
-        bad_pop  = popD[(popD.score <0.5*max_fit)].sequence.tolist()
-        good_pop = popD[(popD.score >=0.5*max_fit) & (popD.score < 0.8*max_fit)].sequence.tolist()
-        for j in range(int(len(bad_pop) / 2) - 1 - int(len(bad_pop)*0.1)):
-            parent_a,parent_b = pair(popD.sequence,popD.score)
+        bad_pop  = popD.sequence.tolist()[:int(cross_rate*len(popD))]
+        good_pop = popD.sequence.tolist()[int(cross_rate*len(popD)):]
+        for j in range(int(len(bad_pop)/2) - 1):
+            parent_a,parent_b = pair(popD.sequence,popD.score.tolist())
             offspring_a,offspring_b = crossover(parent_a,parent_b)
-            offspring_a = mut(offspring_a,restricted_notes,mut_nb,mut_proba)
-            offspring_b = mut(offspring_b,restricted_notes,mut_nb,mut_proba)
+            offspring_a = mut(offspring_a,restricted_notes,mut_nb,mut_tresh)
+            offspring_b = mut(offspring_b,restricted_notes,mut_nb,mut_tresh)
             next_generation += [offspring_a,offspring_b]
-        for g in good_pop:
-            next_generation += [mut(g,restricted_notes,mut_nb*2,mut_proba*2)]
-            
-        n = np.abs(population_size - len(next_generation))
-        population = next_generation + generate_population(n, sequence_length, key_center, restricted_notes=restricted_notes)
         
+        for g in good_pop:
+            next_generation += [mut(g,restricted_notes,mut_nb*2,mut_tresh*2)]
+        n = population_size - len(next_generation)
+        if n > 0:
+            population = next_generation + generate_population(n, sequence_length,
+                                                           key_center, 
+                                                           restricted_notes=restricted_notes) 
+        else:
+            population = next_generation    
+        print(n,len(next_generation))
+        assert len(population) == population_size
     # population = sorted(
     #     population,
     #     key=lambda seq: fitness_func(sequence=seq,functions=fitness_list,key=key_center),
